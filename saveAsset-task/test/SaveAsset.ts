0332d2efeb04b976ae61b44945e1238db817f4a5
AGpeter2018 <adenijipeter2018@gmail.com>
2026-02-25 16:15:15 +0100

    test for other ERC20 interface

--- saveAsset-task/test/SaveAsset.ts
+++ saveAsset-task/test/SaveAsset.ts
@@ -22,6 +22,38 @@ describe("SaveAsset", function () {
     return { owner, user1, user2, token, saveAsset };
   }
 
+  it("Should be contract name", async function() {
+    const {token} = await loadFixture(deployFixture);
+
+    const contractName = await token.name();
+
+    expect(contractName).to.equal("WEB3CXIV")
+  })
+
+  it("Should be symbol", async function() {
+    const {token} = await loadFixture(deployFixture);
+
+    const tokenSymbol = await token.symbol();
+
+    expect(tokenSymbol).to.equal("CXIV");
+  })
+
+  it("Should be decimal", async function() {
+    const {token} = await loadFixture(deployFixture);
+
+    const tokenSymbol = await token.decimals();
+
+    expect(tokenSymbol).to.equal("18");
+  })
+
+  it("Should be totalSupply", async function() {
+    const {token} = await loadFixture(deployFixture);
+
+    const tokenSymbol = await token.totalSupply();
+
+    expect(tokenSymbol).to.equal(ethers.parseEther("2000"));
+  })
+
   it("Should deposit ether", async function () {
     const { user1, saveAsset } = await loadFixture(deployFixture);
     await saveAsset.connect(user1).deposit({ value: ethers.parseEther("1") });
@@ -48,6 +80,49 @@ describe("SaveAsset", function () {
     await saveAsset.connect(user1).depositERC20(ethers.parseEther("100"));
 
     const balance = await saveAsset.connect(user1).getErc20SavingsBalance();
+
     expect(balance).to.equal(ethers.parseEther("100"));
   });
+
+  it("Should withdraw ERC20", async function() {
+    const { saveAsset, user1, token } = await loadFixture(deployFixture);
+
+    await token.connect(user1).approve(saveAsset.target, ethers.parseEther("1"));
+
+    await saveAsset.connect(user1).depositERC20(ethers.parseEther("1"))
+
+    await saveAsset.connect(user1).withdrawERC20(ethers.parseEther("1"));
+
+    const balance = await saveAsset.connect(user1).getErc20SavingsBalance();
+
+    expect(balance).to.equal(ethers.parseEther("0"));
+  })
+
+  it("Should show owner balance", async function() {
+    const {token, user1} = await loadFixture(deployFixture);
+
+    const ownerBalance = await token.balanceOf(user1);
+
+    expect(ownerBalance).to.equal(ethers.parseEther("1000"));
+  })
+
+  it("Should transfer Ether", async function() {
+    const {user1, token, saveAsset} = await loadFixture(deployFixture);
+
+    await token.connect(user1).transfer(saveAsset.target, ethers.parseEther("100"));
+
+    const balance = await saveAsset.connect(user1).getErc20SavingsBalance();
+
+    expect(balance).to.equal(ethers.parseEther("0"));
+  })
+
+  it("should approve spender", async function() {
+    const {token, user1, saveAsset} = await loadFixture(deployFixture);
+
+    await token.connect(user1).approve(saveAsset.target, ethers.parseEther("100"));
+
+    const balance = await saveAsset.connect(user1).getContractBalance();
+
+    expect(balance).to.equal(ethers.parseEther("0"))
+  })
 });
